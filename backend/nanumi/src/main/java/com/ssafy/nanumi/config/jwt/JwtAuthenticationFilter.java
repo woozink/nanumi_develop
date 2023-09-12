4def2f65c8b4e8a8ce91212cc08d3548285ba33b
woozink <k0109974@naver.com>
2023-09-12 15:28:37 +0900

    1

--- backend/nanumi/src/main/java/com/ssafy/nanumi/config/jwt/JwtAuthenticationFilter.java
+++ backend/nanumi/src/main/java/com/ssafy/nanumi/config/jwt/JwtAuthenticationFilter.java
@@ -24,31 +24,34 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        try {
-            String token = jwtProvider.resolveToken(request);
-
-            if (token != null && jwtProvider.validateToken(token)) {
-                // check access token
-                token = token.split(" ")[1].trim();
-                Authentication auth = jwtProvider.getAuthentication(token);
-                SecurityContextHolder.getContext().setAuthentication(auth);
-
-            }
-
-        } catch (MalformedJwtException e) {
-//            log.info("유효하지 않은 토큰입니다.");
-            request.setAttribute("Invalid Token", e);
-        } catch (ExpiredJwtException e) {
-//            log.info("만료된 토큰입니다.");
-            request.setAttribute("Expired Token", e);
-        } catch (UnsupportedJwtException e) {
-//            log.info("지원하지 않는 토큰입니다.");
-            request.setAttribute("Unsupported Token", e);
-        } catch (IllegalStateException e) {
-//            log.info("잘못된 토큰입니다.");
-            request.setAttribute("Wrong Token", e);
-        }
-
         filterChain.doFilter(request, response);
+
+
+//        try {
+//            String token = jwtProvider.resolveToken(request);
+//
+//            if (token != null && jwtProvider.validateToken(token)) {
+//                // check access token
+//                token = token.split(" ")[1].trim();
+//                Authentication auth = jwtProvider.getAuthentication(token);
+//                SecurityContextHolder.getContext().setAuthentication(auth);
+//
+//            }
+//
+//        } catch (MalformedJwtException e) {
+////            log.info("유효하지 않은 토큰입니다.");
+//            request.setAttribute("Invalid Token", e);
+//        } catch (ExpiredJwtException e) {
+////            log.info("만료된 토큰입니다.");
+//            request.setAttribute("Expired Token", e);
+//        } catch (UnsupportedJwtException e) {
+////            log.info("지원하지 않는 토큰입니다.");
+//            request.setAttribute("Unsupported Token", e);
+//        } catch (IllegalStateException e) {
+////            log.info("잘못된 토큰입니다.");
+//            request.setAttribute("Wrong Token", e);
+//        }
+//
+//        filterChain.doFilter(request, response);
     }
 }
